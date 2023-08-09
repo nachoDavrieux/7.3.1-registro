6735e8907de696d45d89ef7f466d2e6d7784b14c
Gaston Nedeff <robertgastonnedeff@gmail.com>
2023-08-09 08:48:27 -0300

    Cambio

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,43 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+function validarFormulario() {
+    const nombre = document.getElementById("nombre").value.trim();
+    const apellido = document.getElementById("apellido").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const contraseña1 = document.getElementById("password1").value;
+    const contraseña2 = document.getElementById("password2").value;
+    const checkboxTerminos = document.getElementById("terminos");
+
+    if (nombre === "" || apellido === "" || email === "" || contraseña1 === "" || contraseña2 === "") {
+        showAlertError(); 
+        return false;
+    }
+
+    if (contraseña1.length < 6) {
+        showAlertError(); 
+        alert("La contraseña debe tener al menos 6 caracteres."); 
+        return false;
+    }
+
+    if (contraseña1 !== contraseña2) {
+        showAlertError(); 
+        alert("Las contraseñas no coinciden. Por favor, ingrésalas de nuevo.");
+        return false;
+    }
+
+    if (!checkboxTerminos.checked) {
+        showAlertError(); 
+        alert("Debes aceptar los términos y condiciones para continuar."); 
+        return false;
+    }
+
+    return true;
+}
+
+document.getElementById("regBtn").addEventListener("click", function() {
+    if (validarFormulario()) {
+        showAlertSuccess(); 
+    }
+});
